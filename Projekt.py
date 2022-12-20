2beb85e6e8a54d37a0d2ea922f9ada56de99e5a5
MasnyBykIByniu <pawel.nowok@edu.uekat.pl>
2022-12-20 15:54:35 +0100

    Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -1,21 +1,37 @@
-import cv2
-import imutils as im
+import tensorflow as tf
+import cv2 as cv
 
-image = cv2.imread(' C :\Projekt\people1.jpg')
-image = im.resize(image, width = min(400, image.shape[1]))
+with tf.gfile.FastGFile('frozen_inference_graph.pb', 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
 
-Czy_czlowiek = cv2.HOGDescriptor()
-# To jest: Histogram zorientowanych gradientów
-Czy_czlowiek .setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-# Ustawiam wspolczynniki dla liniowego klasyfikatora SVM (wprowadzam wspolczynniki klasyfikatora przeszkolonego do wykrywania osob)
-(czlowiek, _) = Czy_czlowiek.detectMultiScale(image, winStride = (3,3), padding = (3,3), scale = 1.01)
-# Wykrywam obiekty o różnych rozmiarach na obrazie wejściowym. Wykryte obiekty są zwracane jako lista prostokątów
+with tf.Session() as sess:
+    sess.graph.as_default()
+    tf.import_graph_def(graph_def, name='')
 
-for (x, y, w, h) in czlowiek:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-# Rysuje prostokat na obrazie (image, start_point, end_pointcolor, thickness)
-cv2.imshow('Czy_czlowiek', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    img = cv.imread('C:/Projekt/people.jpg')
+    rows = img.shape[0]
+    cols = img.shape[1]
+    inp = cv.resize(img, (300, 300))
+    inp = inp[:, :, [2, 1, 0]]
 
-print(f"Liczba ludzi: {len(czlowiek)}")
+    out = sess.run([sess.graph.get_tensor_by_name('num_detections:0'),
+                    sess.graph.get_tensor_by_name('detection_scores:0'),
+                    sess.graph.get_tensor_by_name('detection_boxes:0'),
+                    sess.graph.get_tensor_by_name('detection_classes:0')],
+                   feed_dict={'image_tensor:0': inp.reshape(1, inp.shape[0], inp.shape[1], 3)})
+
+    num_detections = int(out[0][0])
+    for i in range(num_detections):
+        classId = int(out[3][0][i])
+        score = float(out[1][0][i])
+        bbox = [float(v) for v in out[2][0][i]]
+        if score > 0.3:
+            x = bbox[1] * cols
+            y = bbox[0] * rows
+            right = bbox[3] * cols
+            bottom = bbox[2] * rows
+            cv.rectangle(img, (int(x), int(y)), (int(right), int(bottom)), (125, 255, 51), thickness=2)
+
+cv.imshow('TensorFlow MobileNet-SSD', img)
+cv.waitKey()
