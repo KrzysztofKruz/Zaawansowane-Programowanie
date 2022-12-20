f5ade1e3d06a27b7b896b083167520efdc8ea8c7
MasnyBykIByniu <pawel.nowok@edu.uekat.pl>
2022-12-20 15:31:09 +0100

    Projekt.py

--- Projekt.py
+++ Projekt.py
@@ -1,49 +1,21 @@
-import pytesseract.pytesseract as pt
-import cv2 as cv
-import os
+import cv2
+import imutils as im
 
-pt.pytesseract.tesseract_cmd = 'C:\\Users\\student\\AppData\\Local\\Tesseract-OCR\\tesseract.exe'
+image = cv2.imread(' C :\Projekt\people1.jpg')
+image = im.resize(image, width = min(400, image.shape[1]))
 
+Czy_czlowiek = cv2.HOGDescriptor()
+# To jest: Histogram zorientowanych gradientów
+Czy_czlowiek .setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+# Ustawiam wspolczynniki dla liniowego klasyfikatora SVM (wprowadzam wspolczynniki klasyfikatora przeszkolonego do wykrywania osob)
+(czlowiek, _) = Czy_czlowiek.detectMultiScale(image, winStride = (3,3), padding = (3,3), scale = 1.01)
+# Wykrywam obiekty o różnych rozmiarach na obrazie wejściowym. Wykryte obiekty są zwracane jako lista prostokątów
 
-class ImageProcessing:
-    def __init__(self, folder_images_path: str):
-        self.path = folder_images_path
-        self.images = []
+for (x, y, w, h) in czlowiek:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# Rysuje prostokat na obrazie (image, start_point, end_pointcolor, thickness)
+cv2.imshow('Czy_czlowiek', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-    def read_files(self):
-        dir_files = os.listdir(self.path)
-        for file in dir_files:
-            if file[-4:].lower() == 'jpeg':
-                self.images.append(file)
-            elif file[-4:].lower() == '.jpg':
-                self.images.append(file)
-            elif file[-4:].lower() == '.png':
-                self.images.append(file)
-
-        return self.images
-
-    def process_image(self, image_path: str):
-        cvNet = cv.dnn.readNetFromTensorflow('frozen_inference_graph.pb', 'graph.pbtxt')
-        img = cv.imread(f'{self.path}/{image_path}')
-        rows = img.shape[0]
-        cols = img.shape[1]
-        cvNet.setInput(cv.dnn.blobFromImage(img, size=(300, 300), swapRB=True, crop=False))
-        cvOut = cvNet.forward()
-
-        for detection in cvOut[0, 0, :, :]:
-            score = float(detection[2])
-            if score > 0.3:
-                left = detection[3] * cols
-                top = detection[4] * rows
-                right = detection[5] * cols
-                bottom = detection[6] * rows
-                cv.rectangle(img, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=2)
-
-        cv.imshow('img', img)
-        cv.waitKey()
-
-
-processor = ImageProcessing('C:\\Users\\student\\source\\repos\\imageprocessing\\images')
-images = processor.read_files()
-
-print(images)
+print(f"Liczba ludzi: {len(czlowiek)}")
