bd0e32f778aa29af50ad3719b28ce3493f1720b1
KrzysztofKruz <116089024+KrzysztofKruz@users.noreply.github.com>
2023-01-23 20:23:44 +0100

    Create person_detection.py

---
+++ person_detection.py
@@ -0,0 +1,19 @@
+import cv2
+imgage = cv.imread('Zdjecia/Ludzie1.jpg')
+image = cv2.resize(image,(300,300))
+
+Czy_czlowiek = cv2.HOGDescriptor()
+# To jest: Histogram zorientowanych gradientów
+Czy_czlowiek .setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+# Ustawiam wspolczynniki dla liniowego klasyfikatora SVM (wprowadzam wspolczynniki klasyfikatora przeszkolonego do wykrywania osob)
+(czlowiek, _) = Czy_czlowiek.detectMultiScale(image, winStride = (3,3), padding = (3,3), scale = 1.01)
+# Wykrywam obiekty o różnych rozmiarach na obrazie wejściowym. Wykryte obiekty są zwracane jako lista prostokątów
+
+for (x, y, w, h) in czlowiek:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# Rysuje prostokat na obrazie (image, start_point, end_pointcolor, thickness)
+cv2.imshow('Czy_czlowiek', image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print(f"Liczba ludzi: {len(czlowiek)}")
