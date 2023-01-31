fc940d0a5071bec57f9748e81069007e3b771dc4
KrzysztofKruz <116089024+KrzysztofKruz@users.noreply.github.com>
2023-01-31 20:02:21 +0100

    Create TagsLogic.py

---
+++ Model/TagsLogic.py
@@ -0,0 +1,23 @@
+import csv
+import Model.Tag
+
+
+class TagsLogic:
+
+    _tags = []
+
+    def read_tags(self, path: str) -> list:
+        """
+        Funkcja wczytująca dane tagów z bazy danych (pliku .csv)
+        :param path: ścieżka do pliku
+        :return: lista filmów
+        """
+        with open(path, 'r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file, delimiter=',')
+
+            for row in csv_reader:
+                tag = Model.Tag.Tag(row[0], row[1], row[2], row[3])
+                self._tags.append(tag.__dict__)
+
+        self._tags.pop(0)
+        return self._tags
