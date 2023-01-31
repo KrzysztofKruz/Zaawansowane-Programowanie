75f3c1eb69ce1de46f0cb30bb35ef51db1eef7a8
KrzysztofKruz <116089024+KrzysztofKruz@users.noreply.github.com>
2023-01-31 19:16:48 +0100

    Update zad_3.py

--- zad_3.py
+++ zad_3.py
@@ -1,1 +1,13 @@
+def czyParzysta(liczba: int) -> bool:
+    if liczba % 2 == 0:
+        return True
 
+    else:
+        return False
+
+
+wynik = czyParzysta(7)
+if wynik:
+    print('Liczba parzysta')
+else:
+    print('Liczba nieparzysta')
